b8c197caa8ed310dfcc72291d0163a41f10c1fe2
Nikolai Zakharevich <n.zakharevich@corp.vk.com>
2020-10-01 11:26:46 +0300

    f-measure dependence plot

--- knn/knn.py
+++ knn/knn.py
@@ -1,9 +1,11 @@
-from typing import List, NamedTuple
+from typing import List, Mapping, NamedTuple
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-from cf.c import Dist, Kernel, Window, solve
-from utils.data_helper import read_data, vectorize, fill_spaces, normalize, to_list
+from cf.b import solve as cf_b_solve
+from cf.c import Dist, Kernel, Window, solve as cf_c_solve
+from utils.data_helper import read_and_prepare_data
 
 # https://www.openml.org/d/180
 FILE_DATASET_TRAIN = '../datasets/dataset_184_covertype_200.csv'
@@ -17,8 +19,10 @@ class HyperParams(NamedTuple):
     h_or_k: int
 
 
-def read_and_prepare_data(file):
-    return to_list(*normalize(*fill_spaces(*vectorize(read_data(file)))))
+class Data(NamedTuple):
+    d_train: List[List[float]]
+    d_test: List[List[float]]
+    classes: Mapping
 
 
 def get_hyper_params_set() -> List[HyperParams]:
@@ -31,50 +35,64 @@ def get_hyper_params_set() -> List[HyperParams]:
     return result
 
 
-def knn_naive(d_train, hyper_params: HyperParams, q):
+def regression_reduction_naive(d_train: List[List[float]], hyper_params: HyperParams, q: List[float]) -> int:
     dist_type, kernel_type, window_type, h_or_k = hyper_params
 
     if window_type == Window.FIXED:
         h_or_k = h_or_k / 50
 
-    return round(
-        solve(d_train, len(d_train), len(d_train[0]) - 1, q, h_or_k, dist_type, kernel_type, window_type)
-    )
+    return int(round(
+        cf_c_solve(d_train, len(d_train), len(d_train[0]) - 1, q, h_or_k, dist_type, kernel_type, window_type)
+    ))
 
 
-def get_class(row):
+def find_best_hyper_params(data: Data, hyper_params_set: List[HyperParams]):
+    return max(hyper_params_set,
+               key=lambda hp: sum(get_class(q) == regression_reduction_naive(data.d_train, hp, q) for q in data.d_test))
+
+
+def calc_confusion_matrix(data: Data, hyper_params: HyperParams):
+    sz = len(data.classes)
+    matrix = np.zeros((sz, sz))
+    for q in data.d_test:
+        matrix[get_class(q)][regression_reduction_naive(data.d_train, hyper_params, q)] += 1
+    return matrix
+
+
+def show_f_measure_dependence(data: Data, hyper_params: HyperParams):
+    window_width_all = []
+    f_macro_all = []
+    f_micro_all = []
+
+    for h in np.arange(0, 50, 0.5):
+        window_width_all.append(h)
+        f_macro, f_micro = cf_b_solve(list(calc_confusion_matrix(data, hyper_params)), len(data.classes))
+        f_macro_all.append(f_macro)
+        f_micro_all.append(f_micro)
+
+    plt.xlabel('Window width', fontsize=18)
+    plt.ylabel('F-measure', fontsize=16)
+    plt.title('F-measure dependence from window width')
+    plt.plot(window_width_all, f_micro_all, f_macro_all)
+    print(f_micro_all, f_macro_all)
+    plt.show()
+
+
+def get_class(row: list):
     return row[-1]
 
 
-def show_hyper_params(good: list, n: int):
-    plt.xlabel('hyper-parameters index', fontsize=18)
-    plt.ylabel('accuracy', fontsize=16)
-    plt.title('Search for best hyper-parameters set')
-
-    x_all = []
-    y_all = []
-
-    for idx, cnt in enumerate(good):
-        x_all.append(idx)
-        y_all.append(cnt / n)
-
-    print(x_all)
-    print(len(x_all))
-    print(y_all)
-    print(len(y_all))
-    plt.plot(x_all, y_all)
-    # plt.plot(x_all, y_all, 'r.', ms=2)
-    plt.show()
-    print("HERE")
-
-
 def run(file_dataset_train, file_dataset_test):
-    d_train = read_and_prepare_data(file_dataset_train)
-    d_test = read_and_prepare_data(file_dataset_test)
+    d_train, classes = read_and_prepare_data(file_dataset_train)
+    d_test, _ = read_and_prepare_data(file_dataset_test)
+    data = Data(d_train, d_test, classes)
 
-    best_hyper_params = max(get_hyper_params_set(),
-                            key=lambda hp: sum(get_class(q) == knn_naive(d_train, hp, q) for q in d_test))
-    print(best_hyper_params)
+    # best_hyper_params = find_best_hyper_params(data, get_hyper_params_set())
+    best_hyper_params = HyperParams(dist_type=Dist.MANHATTAN, kernel_type=Kernel.UNIFORM, window_type=Window.FIXED,
+                                    h_or_k=3)
+
+    print("Best hyper parameters: ", best_hyper_params)
+    show_f_measure_dependence(data, best_hyper_params)
 
 
 if __name__ == '__main__':
--- utils/data_helper.py
+++ utils/data_helper.py
@@ -3,6 +3,10 @@ import numpy as np
 class_map = {}
 
 
+def read_and_prepare_data(file, with_classes: bool = True):
+    return to_list(*normalize(*fill_spaces(*vectorize(read_data(file), with_classes))))
+
+
 def read_data(file):
     return np.genfromtxt(file, dtype=None, delimiter=',', names=True, encoding='utf-8')
 
@@ -17,33 +21,25 @@ def normalize(data, classes):
     return new_matrix, classes
 
 
-def vectorize(data):
+def vectorize(data, with_classes: bool = False):
     res = []
     classes = []
     for i, x in enumerate(data):
-        if x[-1] not in class_map:
-            class_map[x[-1]] = len(class_map)
-
-        row = []
-        for val in x.tolist()[:-1]:
-            row.append(val)
-        res.append(row)
-        classes.append(class_map[x[-1]])
+        if with_classes:
+            if x[-1] not in class_map:
+                class_map[x[-1]] = len(class_map)
+            row = []
+            for val in x.tolist()[:-1]:
+                row.append(val)
+            res.append(row)
+            classes.append(class_map[x[-1]])
+        else:
+            res.append(x.tolist())
     return np.array(res, dtype=int), np.array(classes)
-
-
-def read_and_prepare_data(file):
-    res = {}
-
-    return to_list(*normalize(*fill_spaces(*vectorize(read_data(file)))))
-
-
-def read_data(file):
-    return np.genfromtxt(file, dtype=None, delimiter=',', names=True, encoding='utf-8')
 
 
 def to_list(data, classes):
     res = []
     for i, x in enumerate(data):
         res.append(x.tolist() + [classes[i]])
-    return res
+    return res, class_map
